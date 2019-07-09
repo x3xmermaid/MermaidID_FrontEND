10cb5469787d0000261320b7fe31915def3adc5c
Dhieo Deva Alavanta <alavanta.deva@gmail.com>
2019-07-09 20:48:18 +0700

    Home Screen Added

---
+++ src/Components/ListCategoryHome.js
@@ -0,0 +1,70 @@
+import React,{Component} from  'react';
+import {View,Text,ScrollView,TouchableOpacity,Image,StyleSheet,AppRegistry} from 'react-native';
+import {categoryHorizontal} from '../Assets/dummy';
+
+class ListCategoryHome extends Component {
+
+    loopCategory(name,image,key){
+        return (
+            <View style={{maxWidth:75,width:75}} key={key}>
+              <TouchableOpacity style={{padding:2}} key={key}>
+                <Image
+                style={style.ImageIconStyle}
+                source={{uri:image}}
+                />
+                <Text style={style.item} numberOfLines={2}>{name}</Text>
+            </TouchableOpacity>
+            </View>
+        )
+    }
+
+    render(){
+        return (
+            <View style={{flex:1,justifyContent:'center',alignContent:'center',maxHeight:90,backgroundColor:'#FFF'}}>
+            <View style={style.scrollViewHolder}>
+            <ScrollView 
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            >
+        {
+            categoryHorizontal.map((item,index) => {
+                return(
+                this.loopCategory(item.name,item.image,item.id)
+                )
+            })
+        }
+            </ScrollView>
+            </View>
+            </View>
+        )
+    }
+}
+
+export default ListCategoryHome;
+
+
+const style=StyleSheet.create({
+    ImageIconStyle: {
+        width:50,
+        height:50,
+
+    },
+    separator: {
+        width: 2,
+        backgroundColor: 'rgba(0,0,0,0.5)'
+    },
+    scrollViewHolder:
+   {
+    marginStart:10,
+    marginEnd:10
+   },
+ 
+   item:
+   {
+      color: 'black',
+      fontSize: 10,
+      padding:7
+   }
+})
+
+//AppRegistry.registerComponent('HorizontalScrollView', () => HorizontalScrollView);
--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text,View,ScrollView} from 'react-native';
+import ListCardItem from '../Components/FlatList';
+import Carousel from '../Components/Carousel';
+import CategoryHorizontal from '../Components/ListCategoryHome';
 
 class Home extends Component {
     render() {
         return(
-            <Text>Ini Home</Text>
-        )
+            <View style={{flex:1,backgroundColor:'#FFF',alignItems:'flex-start',flexDirection:'column'}}>
+            <ScrollView style={{backgroundColor:'#FFF'}}>
+             <Carousel/>
+                <CategoryHorizontal/>
+            <ListCardItem/>
+            </ScrollView>
+            </View>
+            
+        );
     }
 }
 
